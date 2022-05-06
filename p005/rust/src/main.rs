4467f4b96aa1c90af5fa1358fbb7ef7da94e0c97
ORYZAPAO <oryzapao@gmail.com>
2022-05-06 20:51:56 +0900

    1st Commit

---
+++ p005/rust/src/main.rs
@@ -0,0 +1,29 @@
+/// Problem 5 「最小の倍数」 
+/// 2520 は 1 から 10 の数字の全ての整数で割り切れる数字であり, そのような数字の中では最小の値である.
+/// 
+/// では, 1 から 20 までの整数全てで割り切れる数字の中で最小の正の数はいくらになるか
+
+fn mod_chk(val : u32) -> bool{
+    let total = 20;
+
+    for i in 0..total {
+        if val % (total-i) != 0 { return false; }
+    }
+
+    true
+}
+
+fn main() {
+    let mut num = 1;
+
+    loop{
+        if mod_chk(num) {
+            break;
+        }
+        //print!("{} ", num);
+        num += 1;
+    }
+
+    println!("ans = {}", num);
+    
+}
